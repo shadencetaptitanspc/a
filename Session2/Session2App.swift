d358abdb49dc5402c44c0e62a0b29919199e3fdd
use <dsdfd@yhoo.com>
2020-11-26 13:48:09 +0700

    Initial Commit

---
+++ Session2/Session2App.swift
@@ -0,0 +1,17 @@
+//
+//  Session2App.swift
+//  Session2
+//
+//  Created by user on 26.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct Session2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
