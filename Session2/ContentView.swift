cc376ca2886e7a21bc79377b2a1bdeb4c8f0fe1b
use <dsdfd@yhoo.com>
2020-11-26 16:43:51 +0700

    api start

--- Session2/ContentView.swift
+++ Session2/ContentView.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import Alamofire
 
 struct SignUpView:View{
     var body: some View{
         NavigationView{
             ZStack{
-                
+                Color.init("backgroundColor").edgesIgnoringSafeArea(.all)
             }
         }
     }
@@ -19,11 +20,12 @@ struct SignUpView:View{
 
 struct ContentView: View {
     @State var Login: String=""
-    @State var Password:String=""
+    @State var Password:String = ""
+    
+    @State
+    var isAuth: Bool = false
+    
     var body: some View {
-        NavigationView{
-            
-        
         ZStack{
             Color.init("backgroundColor").edgesIgnoringSafeArea(.all)
             VStack{
@@ -32,20 +34,18 @@ struct ContentView: View {
                         .resizable()
                         .frame(width:110,height:120)
                         .padding(.bottom,70)
-                        
+                    
                     Text("Home\nGym")
                         .foregroundColor(.white)
                         .font(.system(size: 35, weight: .regular, design: .default))
                         .padding(.top, 70)
-
+                        
                         .multilineTextAlignment(.center)
                 }
-                
-                
                 ZStack{
                     RoundedRectangle(cornerRadius: 60)
-                    .size(width: 280, height: 40)
-                    .foregroundColor(.white)
+                        .size(width: 280, height: 40)
+                        .foregroundColor(.white)
                         .padding(.leading,15)
                     HStack{
                         Image(systemName:"person.fill")
@@ -53,7 +53,7 @@ struct ContentView: View {
                             .foregroundColor(.gray)
                         Divider()
                         TextField("Ivanov",text:$Login)
-                            
+                        
                     }
                     .padding(.leading,30)
                     
@@ -63,8 +63,8 @@ struct ContentView: View {
                 ZStack{
                     RoundedRectangle(cornerRadius: 60)
                         
-                    .size(width: 280, height: 40)
-                    .foregroundColor(.white)
+                        .size(width: 280, height: 40)
+                        .foregroundColor(.white)
                         .padding(.leading,15)
                     HStack{
                         Image(systemName:"lock.fill")
@@ -73,40 +73,86 @@ struct ContentView: View {
                         Divider()
                             .padding(.leading,3)
                         SecureField("Password",text:$Password)
-                            
+                        
                     }
                     .padding(.leading,30)
                     
                     
                 }
                 .frame(width:310,height:40)
-                ZStack{
+                if self.isAuth {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 60)
+                            .size(width: 280, height: 40)
+                            .foregroundColor(.white)
+                            .padding(.leading,15)
+                        HStack{
+                            Image(systemName:"person.fill")
+                                .font(.system(size:20))
+                                .foregroundColor(.gray)
+                            Divider()
+                            TextField("Ivanov",text:$Login)
+                            
+                        }
+                        .padding(.leading,30)
+                        
+                        
+                    }
+                    .frame(width:310,height:40)
                     ZStack{
                         RoundedRectangle(cornerRadius: 60)
                             
-                        .size(width: 280, height: 40)
-                        .foregroundColor(.white)
+                            .size(width: 280, height: 40)
+                            .foregroundColor(.white)
+                            .padding(.leading,15)
+                        HStack{
+                            Image(systemName:"lock.fill")
+                                .font(.system(size:20))
+                                .foregroundColor(.gray)
+                            Divider()
+                                .padding(.leading,3)
+                            SecureField("Password",text:$Password)
+                            
+                        }
+                        .padding(.leading,30)
+                        
+                        
+                    }
+                    .frame(width:310,height:40)
+                }
+                
+                ZStack{
+                    
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 60)
+                            
+                            .size(width: 280, height: 40)
+                            .foregroundColor(.white)
                             .padding(.leading,15)
                         Button(action:{
-                            print("based")
+                            
                         }){
                             Text("Sign In")
                                 .font(.system(size: 20))
                                 .padding(.bottom,5)
                                 .foregroundColor(Color("backgroundColor"))
                         }
+                        
+                        
                     }
                     .frame(width:310,height:50)
-                    .padding(.top,20)
+                    //.padding(.top,20)
+                    
                     ZStack{
                         Button(action:{
-                            print("based")
+                            self.isAuth = true
                         }){
                             Text("Sign Up")
                                 .foregroundColor(.white)
                                 .underline()
                                 .font(.system(size:20))
-                                .padding(.top,90)
+                                .padding(.top,70)
                         }
                         
                     }
@@ -125,8 +171,7 @@ struct ContentView: View {
                     }
                     
                 }
-                .padding(.top,50)
-            }
+                .padding(.top,40)
             }
         }
     }
@@ -137,3 +182,13 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct Item:Codable{
+    var notice:JOP
+}
+
+struct JOP:Codable{
+    var token: Int
+}
+
